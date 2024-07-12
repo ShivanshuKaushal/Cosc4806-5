12a66218797cca3b369bf6360885589a9bd68877
ShivanshuKaushal <shivanshu02@algomau.ca>
2024-07-12 22:30:36 +0000

    added breadcrum to header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,6 +6,18 @@ if (!isset($_SESSION['auth'])) {
 <!DOCTYPE html>
 <html lang="en">
     <head>
+      <div class="container mt-3">
+          <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+                  <?php if ($_SESSION['controller'] == 'home'): ?>
+                      <li class="breadcrumb-item active" aria-current="page">Home /</li>
+                  <?php else: ?>
+                      <li class="breadcrumb-item"><a href="/">Home</a></li>
+                      <li class="breadcrumb-item active" aria-current="page"><?php echo ucwords($_SESSION["controller"]); ?></li>
+                  <?php endif; ?>
+              </ol>
+          </nav>
+      </div>
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="icon" href="/favicon.png">
         <title>COSC 4806</title>
--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -8,10 +8,24 @@ if (isset($_SESSION['auth']) == 1) {
 <html lang="en">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="/favicon.png">
-    <title>COSC 4806</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">COSC 4806</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    </ul>
+                    <div class="d-flex ms-auto">
+                        <a class="nav-link text-white" href="/login">Login</a>
+                        <a class="nav-link text-white" href="/create">Create account</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+    </html>
 </head>
 <body>
